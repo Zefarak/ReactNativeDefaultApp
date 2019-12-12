fb3bd691c72a6118b61968665d9f39673b43577a
chris <lirageika#hotmail.gr>
2019-12-12 11:06:17 +0200

    added plan

--- src/Homepage/WorkoutDetailScreen.js
+++ src/Homepage/WorkoutDetailScreen.js
@@ -1,8 +1,14 @@
 import React, {Component} from 'react';
-import {View, Text, StyleSheet} from 'react-native';
-import {Card, ListItem} from 'react-native-elements';
-import { WORKOUTS_ENDPOINT, WORKOUT_DETAIL_ENDPOINT, WORKOUT_PARTS_LIST_ENDPOINT } from '../constants/endpoints';
-import { lookupPublicOptions } from '../Account/auth';
+import {View, Text, StyleSheet, Button, AsyncStorage} from 'react-native';
+import {Card, ListItem, Overlay} from 'react-native-elements';
+import {
+    WORKOUTS_ENDPOINT,
+    WORKOUT_DETAIL_ENDPOINT,
+    WORKOUT_PARTS_LIST_ENDPOINT,
+    PLAN_LIST_ENDPOINT
+} from '../constants/endpoints';
+import {lookupOptionsGETWithToken, lookupPublicOptions} from '../Account/auth';
+import MyButton from "../components/Button";
 
 const list = [
     {
@@ -15,18 +21,27 @@ const list = [
       avatar_url: 'https://s3.amazonaws.com/uifaces/faces/twitter/adhamdannaway/128.jpg',
       subtitle: 'Vice Chairman'
     },
-    
-  ]
+
+  ];
 
 
 class WorkoutDetailScreen extends Component {
     constructor(props){
-        super(props)
+        super(props);
         this.state = {
             workout: null,
             doneLoading: false,
             parts:[],
-            doneLoadingParts: false
+            doneLoadingParts: false,
+            exercises:[],
+
+            isVisible: false,
+            overlay_data:{},
+
+            login_message: false,
+            plans: [],
+            doneLoadingPlans: false
+
         }
     }
 
@@ -42,72 +57,108 @@ class WorkoutDetailScreen extends Component {
             })
         })
         .catch(error=>{
-            console.log('error', error)
+            console.log('error', error);
             alert('Something is Wrong, Nio')
         })
     }
 
-    loadWorkoutParts(id){
-        const thisComp = this;
-        const endpoint = WORKOUT_PARTS_LIST_ENDPOINT + '?workout_related='+ id;
-        fetch(endpoint, lookupPublicOptions)
-        .then(resp=>resp.json())
-        .then(respData=>{
-            thisComp.setState({
-                parts: respData,
-                doneLoadingParts: true
-            })
-        })
-        .catch(error=>{
-            console.log('part error', error)
+    handleOverlay = (exercise) =>{
+        this.setState({
+            isVisible: true,
+            overlay_data:exercise
         })
     };
 
+    loadPlans = () => {
+        const thisComp = this;
+        const endpoint = PLAN_LIST_ENDPOINT + '?active=true';
+        const token = AsyncStorage.getItem('access');
+        fetch(endpoint, lookupOptionsGETWithToken(token))
+            .then(resp=>resp.json())
+            .then(respData=>{
+                console.log(respData);
+                if(respData.code !== undefined) {
+                    console.log('here!');
+                    thisComp.setState({
+                        login_message:true,
+                        doneLoadingPlan: true
+                    })
+                } else {
+                    this.setState({
+                        plans: respData.result,
+                        doneLoadingPlan: true
+                    })
+                }
+            })
+    };
+
     componentDidMount(){
-        const id = JSON.stringify(this.props.navigation.getParam('id', 1))
-        console.log('id', id)
+        const id = JSON.stringify(this.props.navigation.getParam('id', 1));
         this.loadWorkout(id)
     }
 
     render(){
-        const {workout, parts, doneLoadingParts, doneLoading } = this.state;
+        const {workout, doneLoading, overlay_data, doneloadingPlans, plans, login_message } = this.state;
+        console.log(overlay_data);
         return (
-            <View>
+            <View style={styles.screen}>
                 {doneLoading ?
                     <Card title={workout.title}>
                         <Text>{workout.guide}</Text>
-                        
-                        <View
-                            style={{ flex: 1, flexDirection: 'row', justifyContent: 'space-between' }}
-                        >
+
+                        <View style={styles.secondInfo}>
                             <Text>{workout.tag_category}</Text>
-                <Text>{workout.tag_user}</Text>
+                            <Text>By {workout.tag_user}</Text>
                         </View>
                         <View>
                             {
-                                list.map((l, i) => (
+                                workout.tag_parts.map((l, i) => (
                                 <ListItem
                                     key={i}
-                                    leftAvatar={{ source: { uri: l.avatar_url } }}
-                                    title={l.name}
-                                    subtitle={l.subtitle}
+                                    title={l.exercise_related__title}
+                                    subtitle={l.guide}
+                                    onPress={()=> {this.handleOverlay(l)}}
                                     bottomDivider
                                 />
                                 ))
                             }
+                        </View>
+                        <Button title='Add To Plan' onPress={this.loadPlans} />
+                        {login_message?
+                            <View>
+                                {doneloadingPlans ?
+                                    <Text>Data!</Text>
+                                    :
+                                    <MyButton title='Login'/>
+                                }
                             </View>
-
-                    </Card>   
+                        :<View><Text>You need to login</Text></View>}
+                    </Card>
             :
             <Text>Loading....</Text>
             }
+
+            <Overlay
+                isVisible={this.state.isVisible}
+                onBackdropPress={() => this.setState({ isVisible: false })}
+                >
+                <Text>{overlay_data.exercise_related__title}</Text>
+            </Overlay>
             </View>
         )
     }
 }
 
 const styles = StyleSheet.create({
-    
+    screen: {
+        flex:1,
+
+    },
+    secondInfo: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        marginBottom: 10
+    }
 })
 
 export default WorkoutDetailScreen;
--- src/constants/endpoints.js
+++ src/constants/endpoints.js
@@ -3,7 +3,7 @@ const SITE = 'https://backend-workout-server.herokuapp.com/api/';
 export const TOKEN_ENDPOINT = SITE + 'token/';
 export const REFRESH_TOKEN = SITE + 'token/refresh/';
 export const REGISTER_ENDPOINT = SITE + 'accounts/create/user/';
-export const CURRENT_USER_ENDPOINT = SITE + 'accounts/current-user/'
+export const CURRENT_USER_ENDPOINT = SITE + 'accounts/current-user/';
 
 export const EXERCISE_CATEGORIES_ENDPOINT = SITE + 'exercises/category/list/';
 export const EXERCISES_LIST_ENDPOINT = SITE + 'exercises/list/';
@@ -13,3 +13,6 @@ export const WORKOUTS_ENDPOINT = SITE + 'workouts/list/';
 export const WORKOUTS_CREATE = SITE + 'workouts/create/';
 export const WORKOUT_DETAIL_ENDPOINT = SITE + 'workouts/retrieve-update-delete/';
 export const WORKOUT_PARTS_LIST_ENDPOINT = SITE + 'workouts/workout-part/list/';
+
+
+export const PLAN_LIST_ENDPOINT = SITE + 'planner/list/';
